f50d2106e597058869d32d379872372073f3e74a
Jamie Kerber <kerber.jg@gmail.com>
2025-05-14 21:51:20 +0200

    [filament_view] Fixed runtime_assert missing std:string overload

--- plugins/filament_view/core/utils/asserts.h
+++ plugins/filament_view/core/utils/asserts.h
@@ -1,10 +1,16 @@
 #pragma once
 
+#include <string>
+
 namespace plugin_filament_view {
 
 /// Convenience function to assert a condition at runtime.
 /// Throws a runtime_error with a given message if the condition is false.
 void runtime_assert(bool condition, const char* message);
+
+inline void runtime_assert(bool condition, const std::string& message) {
+  runtime_assert(condition, message.c_str());
+}
 
 #define debug_assert(condition, message) \
     assert(condition && message);
